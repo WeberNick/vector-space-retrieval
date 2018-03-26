083546dee2a1be9cb75da7ea22bf1e35e1f15761
WeberNick <weber.nick@icloud.com>
2018-03-26 12:41:24 +0200

    Basic Project structure

---
+++ src/infra/types.hh
@@ -0,0 +1,9 @@
+#include <cstdint>
+#include <cstddef>
+#include <iostream>
+#include <vector>
+
+typedef std::size_t     size_t
+typedef std::byte       byte;
+typedef unsigned int    uint;
+
---
+++ src/main/main.cc
@@ -0,0 +1,8 @@
+#include "infra/types.hh"
+
+
+int main(const int argc, const char* argv[])
+{
+
+    return 0;
+}
