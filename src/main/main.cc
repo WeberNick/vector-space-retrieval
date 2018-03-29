95113ebb35d8bf20975005801f333f8eaa8cdf55
Alexander Weiß <weiss_alex@gmx.net>
2018-03-29 18:24:54 +0200

    Merge

--- src/main/main.cc
+++ src/main/main.cc
@@ -1,5 +1,6 @@
 #include "../document.hh"
 #include "../document_manager.hh"
+#include "../infra/similarity_measures.hh"
 #include "../infra/types.hh"
 
 int main(const int argc, const char* argv[]) {
@@ -22,6 +23,13 @@ int main(const int argc, const char* argv[]) {
 
   // doc parser here, replace "dev.txt"
   // DocumentManager* docManager = new DocumentManager("dev.txt");
+
+  std::vector<double> doc_a = {1,3,5,8, 100, 100};
+  std::vector<double> doc_b = {2,4,5,1,2,0};
+
+  std::cout << similarity_measures::tf_idf(5.0, 10.0) << std::endl;
+  std::cout << similarity_measures::euclidean_distance_normalized(doc_a, doc_b) << std::endl;
+  std::cout << similarity_measures::cosine_similarity(doc_a, doc_b) << std::endl;
   std::cout << "Hello CMake" << std::endl;
   return 0;
 }
