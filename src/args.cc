58fd74b8cbe072b54cac5459da1d7e0d45a2430f
WeberNick <weber.nick@icloud.com>
2018-04-09 20:53:47 +0200

    small fixes to comply with compiler warnings

--- src/args.cc
+++ src/args.cc
@@ -1,11 +1,11 @@
 #include "args.hh"
 
 void construct_arg_desc(argdesc_vt& x) {
-    typedef argdesc_t<Args, char> carg_t;
-    typedef argdesc_t<Args, int> iarg_t;
-    typedef argdesc_t<Args, uint> uarg_t;
-    typedef argdesc_t<Args, float> farg_t;
-    typedef argdesc_t<Args, double> darg_t;
+    //typedef argdesc_t<Args, char> carg_t;
+    //typedef argdesc_t<Args, int> iarg_t;
+    //typedef argdesc_t<Args, uint> uarg_t;
+    //typedef argdesc_t<Args, float> farg_t;
+    //typedef argdesc_t<Args, double> darg_t;
     typedef argdesc_t<Args, bool> barg_t;
     typedef argdesc_t<Args, std::string> sarg_t;
 
@@ -16,6 +16,6 @@ void construct_arg_desc(argdesc_vt& x) {
     x.push_back(new sarg_t("--path", "../data/", &Args::path, "path to the data directory. relative path from folder 'src' or absolute path possible"));
 }
 
-Args::Args() : _help(false), _trace(false), _measure(false), _print(false), _path("../data/") {}
+Args::Args() : _help(false), _trace(false), _measure(false), _print(false), _path("./data/") {}
 
 Args::~Args() {}
