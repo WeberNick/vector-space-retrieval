04fe0bc8e915309381f332f849013882daa0c1ad
Nicolas Wipfler <nicolas.wipfler@gmx.de>
2018-05-06 16:51:08 +0200

    Fixed preprocessing

--- python/preprocessing/preprocess.py
+++ python/preprocessing/preprocess.py
@@ -1,56 +1,60 @@
 ''' Preprocessing module '''
 import re
+import string
 
 from natsort import natsorted
 from nltk.stem.snowball import SnowballStemmer
 
-DATA_PATH = '../data/'
-CORPUS_PATH = '../data/nfcorpus/' # location of unprocessed corpus; not pushed to remote server
+DATA_PATH = '../../data'
+RAW_FOLDER = 'raw'
 
-def qrel(files, ending):
+def qrel(folders, files, end):
     ''' Aggregate query relevance '''
-    for end in ending:
-        result = set()
+    for folder in folders:
         for filename in files:
-            with open(CORPUS_PATH + filename + '.' + end) as file_from:
+            result = set()
+            with open('{}/{}/{}/{}.{}'.format(DATA_PATH, folder, RAW_FOLDER, filename, end)) as file_from:
                 for line in file_from.readlines():
                     content = re.split(r'\t+', line.rstrip('\t\n'))
                     del content[1]
                     result.add('~'.join(content))
-        _write(result, end)
+            _write(result, '{}/{}/{}.{}'.format(DATA_PATH, folder, filename, end))
 
-def docs(files, end):
+def docs(folders, filename, end):
     ''' Aggregate document collection '''
     stemmer = SnowballStemmer(language='english')
-    result = set()
-    for filename in files:
-        with open(CORPUS_PATH + filename + '.' + end) as file_from:
+    regex = re.compile('[%s]' % re.escape(string.punctuation))
+    for folder in folders:
+        result = set()
+        with open('{}/{}/{}/{}.{}'.format(DATA_PATH, folder, RAW_FOLDER, filename, end)) as file_from:
             for line in file_from.readlines():
                 content = re.split(r'\t+', line.rstrip('\t\n'))
-                words = ' '.join([stemmer.stem(word) for word in content[1].split()])
-                doc = [content[0], words]
+                words = [regex.sub('', w) for w in content[1].split()]
+                words = [stemmer.stem(w) for w in words]
+                words = [w for w in words if len(w) > 1]
+                doc = [content[0], ' '.join(words)]
                 result.add('~'.join(doc))
-    _write(result, 'collection.{}'.format(end))
+        _write(result, '{}/{}/{}.{}'.format(DATA_PATH, folder, filename, end))
 
-def quer(files, types, end):
+def quer(folders, files, end):
     ''' Aggregate queries '''
-    for typ in types:
-        result = set()
+    for folder in folders:
         for filename in files:
-            with open('{}{}.{}.{}'.format(CORPUS_PATH, filename, typ, end)) as file_from:
+            result = set()
+            with open('{}/{}/{}/{}.{}'.format(DATA_PATH, folder, RAW_FOLDER, filename, end)) as file_from:
                 for line in file_from.readlines():
                     content = re.split(r'\t+', line.rstrip('\t\n'))
                     result.add('~'.join(content))
-        _write(result, '{}.{}'.format(typ, end))
+            _write(result, '{}/{}/{}.{}'.format(DATA_PATH, folder, filename, end))
 
 def _write(result_set, filename):
     ''' Write results to a file '''
-    with open(DATA_PATH + filename, 'w') as file_to:
+    with open(filename, 'w') as file_to:
         for line in natsorted(list(result_set), key=lambda x: x.lower()):
             file_to.write('{}\n'.format(line))
 
 if __name__ == '__main__':
-    shape = ['test', 'dev', 'train']
-    #qrel(shape, ['3-2-1.qrel', '2-1-0.qrel'])
-    #docs(shape, 'docs')
-    quer(shape, ['all', 'titles', 'nontopic-titles', 'vid-titles', 'vid-desc'], 'queries')
+    folders = ['test', 'dev', 'train']
+    qrel(folders, ['s-3', 's-4'], 'qrel')
+    docs(folders, 'd-collection', 'docs')
+    quer(folders, ['q-all', 'q-titles', 'q-nontopictitles', 'q-vidtitles', 'q-viddesc'], 'queries')
