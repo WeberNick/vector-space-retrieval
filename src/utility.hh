91370d5517dd7870070556b2f1f7563e2ec24102
Alexander Weiß <weiss_alex@gmx.net>
2018-05-09 16:28:49 +0200

    Revert mergePostingLists(std::vector<sizet_vt>& vecs, sizet_vt& out)

--- src/utility.hh
+++ src/utility.hh
@@ -599,18 +599,14 @@ namespace Utility {
          */
         inline void mergePostingLists(std::vector<sizet_vt>& vecs, sizet_vt& out) {
 
-            if (vecs.size() > 1) {
-                for (auto& elem : vecs) {
-                    std::sort(elem.begin(), elem.end(), [](const size_t& a, const size_t& b) { return a < b; }); // asc
-                }
-
+           if (vecs.size() > 1) {
+                std::sort(vecs.begin(), vecs.end(), [](const sizet_vt& a, const sizet_vt& b) { return a.size() < b.size(); }); // asc
                 out.clear();
                 mergePostingLists(vecs.at(0), vecs.at(1), out);
                 if (vecs.size() == 2) return;
                 for (size_t i = 2; i < vecs.size(); ++i) {
                     sizet_vt out_copy(out);
                     mergePostingLists(out_copy, vecs.at(i), out);
-                    std::cout << "posting merge end" << std::endl;
                 }
             } else {
                 out = vecs.at(0);
